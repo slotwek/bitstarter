cbfcee3bdf254d348d7ce50e9c0d0bfd7aa42f1a
Lukasz Slotwinski <mail.Slotwinski@gmail.com>
2013-07-08 21:18:35 +0000

    First attempt to HW 3 - parts 2

--- web.js
+++ web.js
@@ -3,8 +3,22 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  var fs = require('fs');
+  var fileName = fs.readFileSync("index.html");
+  var buf = new Buffer(fileName);
+  /*
+  for (var i = 0; i < fileName.length; i++) {
+    buf[i] = str.charCodeA(i);
+  }
+  */
+  resonse.send(buf.toString());
+});
+
+/*
+app.get('/', function(request, response) {
   response.send('Hello World 2!');
 });
+*/
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
