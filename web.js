39a77ceac84482d04e6de44ee2b5fb5e912876bb
Lukasz Slotwinski <mail.Slotwinski@gmail.com>
2013-07-08 21:33:35 +0000

    Another change to web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require('fs');
-  var buf = new Buffer(1000);
+  //var buf = new Buffer(1000);
   var fileName = fs.readFileSync("index.html", "utf-8");
   /*
   for (var i = 0; i < fileName.length; i++) {
